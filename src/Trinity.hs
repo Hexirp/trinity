c6952d24820bd7dd09048ad5dfd36438ab4e0ab9
Hexirp <Hexirp@users.noreply.github.com>
2021-03-20 18:16:07 +0900

    Update: 色々なインスタンスを導出させる
    binary のインスタンスにするための準備として。

--- src/Trinity.hs
+++ src/Trinity.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+
 module Trinity where
   import Prelude
+
+  import GHC.Generics
 
   import qualified Data.Set as Set
 
@@ -11,16 +17,31 @@ module Trinity where
   import qualified Data.UUID.V4 as UUID
 
   newtype ID = ID UUID.UUID
+    deriving stock Eq
+    deriving stock Ord
+    deriving stock Show
+    deriving stock Read
+    deriving stock Generic
 
   generate_id :: IO ID
   generate_id = ID <$> UUID.nextRandom
 
   newtype Time = Time Time.UTCTime
+    deriving stock Eq
+    deriving stock Ord
+    deriving stock Show
+    deriving stock Read
+    deriving stock Generic
 
   get_current_time :: IO Time
   get_current_time = Time <$> Time.getCurrentTime
 
   newtype Object = Object Byte.ByteString
+    deriving stock Eq
+    deriving stock Ord
+    deriving stock Show
+    deriving stock Read
+    deriving stock Generic
 
   data Triple
     =
@@ -33,6 +54,11 @@ module Trinity where
           triple_time :: !Time,
           triple_author :: !ID
         }
+    deriving stock Eq
+    deriving stock Ord
+    deriving stock Show
+    deriving stock Read
+    deriving stock Generic
 
   data Model
     =
@@ -41,6 +67,11 @@ module Trinity where
           model_id_set :: !(Set.Set ID),
           model_triple_set :: !(Set.Set Triple)
         }
+    deriving stock Eq
+    deriving stock Ord
+    deriving stock Show
+    deriving stock Read
+    deriving stock Generic
 
   generate_initial_model :: IO Model
   generate_initial_model = do
